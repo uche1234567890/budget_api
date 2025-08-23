857cf855d60cf93413b13b639794d3bf6c318423
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-08-23 06:26:51 +0100

    Added API views for Category and Transaction

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Transaction
+from .serializers import CategorySerializer, TransactionSerializer
 
 # Create your views here.
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class TransactionListCreateView(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
