2ee79042ae17951cae790036fd9f01366f9611bd
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-08-30 12:00:37 +0100

    feat(auth): add RegisterSerializer and LoginSerializer for user signup and login handling

--- budget/serializers.py
+++ budget/serializers.py
@@ -1,16 +1,39 @@
 from rest_framework import serializers
 from .models import Category, Transaction
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        # don't expose user, since it's auto-filled in perform_create
         fields = ["id", "name", "created_at"]
-
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        # don't expose user, handled automatically
         fields = ["id", "amount", "type", "category", "created_at"]
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password")
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"]
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data["username"], password=data["password"])
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
