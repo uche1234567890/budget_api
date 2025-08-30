b2085d19a293e2696e076b74ff5ae115fe01553e
uche Nwankwo <ucheo.nwankwo@gmail.com>
2025-08-30 20:57:53 +0100

    feat(auth): create RegisterView and LoginView using DRF generics and JWT tokens

--- budget/views.py
+++ budget/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.db.models import Sum
+from django.contrib.auth.models import User
 from .models import Category, Transaction
-from .serializers import CategorySerializer, TransactionSerializer
-
+from .serializers import CategorySerializer, TransactionSerializer , RegisterSerializer, LoginSerializer
 
 class CategoryListCreateView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
@@ -67,3 +67,20 @@ class SummaryView(APIView):
             "balance": balance,
             "by_category": category_summary
         })
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        tokens = serializer.get_tokens_for_user(user)
+        return Response(tokens, status=status.HTTP_200_OK)
